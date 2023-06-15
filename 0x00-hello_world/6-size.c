f31eee6cff3a31df64d2e06aaa70127dff72e94b
delta-az <azedinedelta@gmail.com>
2023-06-15 16:36:31 -0700

    f

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-int a;
-char b;
-long int c;
-long long int d;
-float e;
+	int a;
+	char b;
+	long int c;
+	long long int d;
+	float e;
 printf("Size of char: %zu byte(s)\n", sizeof(b)(char));
 printf("Size of int: %zu byte(s)\n", sizeof(a)(int));
 printf("Size of long: %zu byte(s)\n", sizeof(c)(long int));
