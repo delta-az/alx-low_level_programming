883f7a1fb97f09955c04da02d5cca75f621369ef
delta-az <azedinedelta@gmail.com>
2023-06-15 17:12:27 -0700

    t

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	write("and that piece of art is useful\" – Dora korpar, 2015-10-19\", stdout);
+	write("and that piece of art is useful\" – Dora korpar, 2015-10-19\");
 return (1);
 }
