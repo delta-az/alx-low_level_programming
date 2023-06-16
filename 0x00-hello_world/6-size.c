5e8d8c965578a3c715f318d00e2f807a31603a42
delta-az <azedinedelta@gmail.com>
2023-06-15 17:26:27 -0700

    t

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,11 @@
  */
 int main(void)
 {
-printf("Size of a char:%d byte(s)\n",sizeof(char));
-printf("Size of an int:%d byte(s)\n",sizeof(int));
-printf("Size of a long:%d byte(s)\n",sizeof(long int));
-printf("Size of a long long:%d byte(s)\n",sizeof(long long int));
-printf("Size of a float:%d byte(s)\n",sizeof(float));
+	long long int D;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(D));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
 return (0);
 }
