88ab2d688035d81345b2fe0fc94c638087fffcf0
delta-az <azedinedelta@gmail.com>
2023-06-21 16:28:13 -0700

    t

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,2 +1,4 @@
 void positive_or_negative(int i);
-#include <stdio.h>
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
